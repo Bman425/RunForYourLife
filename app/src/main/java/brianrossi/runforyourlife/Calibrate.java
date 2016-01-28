021489828fc01741564f246f0b998f410a92682c
Ben Rushin <benjaminprushin@gmail.com>
2016-01-28 17:41:11 -0500

    Re arranged assignments to avoid errors

--- app/src/main/java/brianrossi/runforyourlife/Calibrate.java
+++ app/src/main/java/brianrossi/runforyourlife/Calibrate.java
@@ -1,9 +1,11 @@
 package brianrossi.runforyourlife;
 
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,20 +13,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class Calibrate extends AppCompatActivity implements SensorEventListener {
+import java.util.jar.Manifest;
 
-    SensorManager hrmManager ;
-    Sensor hrmSensor;
-    private static final String TAG = "MainActivity";
+public class Calibrate extends AppCompatActivity implements SensorEventListener, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    SensorManager hrmManager ;  //Declares Sensor manager field
+    Sensor hrmSensor;  // Declares Sensor field
+    private static final String TAG = "MainActivity";  //Declars Tag for log, probably should be better defined
     private TextView mTextViewHeart;
+    private static final int PERM_REQUEST_HRM = 1;  //ID for permision request call, not used
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hrmManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        hrmSensor = hrmManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        //requestPermissions(new String[]{android.Manifest.permission.BODY_SENSORS}, PERM_REQUEST_HRM);  //Requires Android 6
+        hrmManager = (SensorManager)getSystemService(SENSOR_SERVICE);  //Sets the manager
+        //hrmSensor = hrmManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);  //sets the sensor
+        hrmSensor = hrmManager.getDefaultSensor(65562);  //Attempt to get sensor
         setContentView(R.layout.activity_calibrate);
-        hrmManager.registerListener((SensorEventListener) this, hrmSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        //mTextViewHeart = (TextView) findViewById(R.id.heart);
+        hrmManager.registerListener((SensorEventListener) this, hrmSensor, SensorManager.SENSOR_DELAY_NORMAL);  //registers as listener to sensor
+
     }
 
     @Override
@@ -50,18 +57,23 @@ public class Calibrate extends AppCompatActivity implements SensorEventListener 
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent event) {  //Logs sensor updates
         if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
             String msg = "" + (int)event.values[0];
             mTextViewHeart.setText(msg);
             Log.d(TAG, msg);
-            System.out.println(msg);
         }
         else
             Log.d(TAG, "Unknown sensor type");
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {  //Logs changes in sensor accuaracy
         Log.d(TAG, "onAccuracyChanged - accuracy: " + accuracy);
     }
+    /*public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if (requestCode == PERM_REQUEST_HRM && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            hrmManager.registerListener((SensorEventListener) this, hrmSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            //mTextViewHeart = (TextView) findViewById(R.id.heart);
+        }
+    }*/
 }
