5f344a40e3265a1d42218ea55efefad329dbe4c1
Ben Rushin <benjaminprushin@gmail.com>
2016-02-02 21:27:27 -0500

    Updated to samsung digital Health

--- app/src/main/java/brianrossi/runforyourlife/Calibrate.java
+++ app/src/main/java/brianrossi/runforyourlife/Calibrate.java
@@ -13,24 +13,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionKey;
+
+import java.util.Set;
 import java.util.jar.Manifest;
 
-public class Calibrate extends AppCompatActivity implements SensorEventListener, ActivityCompat.OnRequestPermissionsResultCallback {
+public class Calibrate extends AppCompatActivity implements HealthDataStore.ConnectionListener {
+    private Set<PermissionKey> mKeySet;
+    private HealthPermissionManager PermManag;
+    private HealthDataStore HealthStore;
 
-    SensorManager hrmManager ;  //Declares Sensor manager field
-    Sensor hrmSensor;  // Declares Sensor field
-    private static final String TAG = "MainActivity";  //Declars Tag for log, probably should be better defined
+    private static final String TAG = "Calibrate";  //Declars Tag for log, probably should be better defined
     private TextView mTextViewHeart;
-    private static final int PERM_REQUEST_HRM = 1;  //ID for permision request call, not used
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestPermissions(new String[]{android.Manifest.permission.BODY_SENSORS}, PERM_REQUEST_HRM);  //Requires Android 6
-        hrmManager = (SensorManager)getSystemService(SENSOR_SERVICE);  //Sets the manager
-        //hrmSensor = hrmManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);  //sets the sensor
-        hrmSensor = hrmManager.getDefaultSensor(65562);  //Attempt to get sensor
+        HealthStore = new HealthDataStore(this, this);
+        PermManag = new HealthPermissionManager(HealthStore);
         setContentView(R.layout.activity_calibrate);
-        hrmManager.registerListener((SensorEventListener) this, hrmSensor, SensorManager.SENSOR_DELAY_NORMAL);  //registers as listener to sensor
+
 
     }
 
@@ -57,23 +61,17 @@ public class Calibrate extends AppCompatActivity implements SensorEventListener,
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {  //Logs sensor updates
-        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-            String msg = "" + (int)event.values[0];
-            mTextViewHeart.setText(msg);
-            Log.d(TAG, msg);
-        }
-        else
-            Log.d(TAG, "Unknown sensor type");
+    public void onConnected() {
+
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {  //Logs changes in sensor accuaracy
-        Log.d(TAG, "onAccuracyChanged - accuracy: " + accuracy);
+    @Override
+    public void onConnectionFailed(HealthConnectionErrorResult healthConnectionErrorResult) {
+
     }
-    /*public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        if (requestCode == PERM_REQUEST_HRM && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            hrmManager.registerListener((SensorEventListener) this, hrmSensor, SensorManager.SENSOR_DELAY_NORMAL);
-            //mTextViewHeart = (TextView) findViewById(R.id.heart);
-        }
-    }*/
+
+    @Override
+    public void onDisconnected() {
+
+    }
 }
